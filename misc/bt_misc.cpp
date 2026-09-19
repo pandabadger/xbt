ae6a41d7d26e393b988716e2638547139ac8179d
pandabadger <33740825+pandabadger@users.noreply.github.com>
2026-09-20 00:33:22 +0100

    move is_ipv4

--- misc/bt_misc.cpp
+++ misc/bt_misc.cpp
@@ -176,6 +176,22 @@ std::string uri_encode(str_ref v)
 	return r;
 }
 
+bool is_ipv4(const std::array<unsigned char, 16>& v)
+{
+    return v[0] == 0
+           && v[1] == 0
+           && v[2] == 0
+           && v[3] == 0
+           && v[4] == 0
+           && v[5] == 0
+           && v[6] == 0
+           && v[7] == 0
+           && v[8] == 0
+           && v[9] == 0
+           && v[10] == 0xff
+           && v[11] == 0xff;
+}
+
 bool is_private_ipa(int a)
 {
 	return (ntohl(a) & 0xff000000) == 0x0a000000
